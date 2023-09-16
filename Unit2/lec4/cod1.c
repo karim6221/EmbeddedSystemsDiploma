4b561a50fc362de43db57c3eb2ca4c8b47a83a11
karim ashrf <ashrfkarim622@gmail.com>
2023-09-16 15:53:56 +0300

    hw3-cod1

---
+++ Unit2/lec4/cod1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    float a[2][2],b[2][2];
+    printf("enter the elemnts of 1st matrix\n");
+    for(int i = 0; i < 2 ; i++)
+    {
+        for(int j = 0; j < 2 ; j++)
+        {
+            printf("enter a%d%d: ",i+1,j+1);
+            scanf("%f",&a[i][j]);
+        }
+    }
+    printf("enter the elemnts of 2nd matrix\n");
+    for(int w = 0; w < 2 ; w++)
+    {
+        for(int e = 0; e < 2 ; e++)
+        {
+            printf("enter b%d%d: ",w+1,e+1);
+            scanf("%f",&b[w][e]);
+        }
+    }
+    printf("sum of matrix\n");
+    for(int r = 0; r < 2 ; r++)
+    {
+        for(int t = 0; t < 2 ; t++)
+        {
+            printf("%0.1f\t",a[r][t]+b[r][t]);
+        }
+        printf("\n");
+    }
+    
+}
